da82baf8999848e94d562b3171199531fb262b29
vbendre <vbendre@ebates.com>
2020-05-04 09:54:35 -0700

    added channel commnication code

--- status-checker/main.go
+++ status-checker/main.go
@@ -15,20 +15,30 @@ func main() {
 		"http://stackoverflow.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		fmt.Println("checking link --" + link)
-		go checkLink(link)
+		// fmt.Println("checking link --" + link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link + " might be down!!")
+		c <- "might be down I think"
 		return
 	}
 
 	fmt.Println(link + " link is working!!")
+	c <- "yeah its up"
 
 }
