c826afe438cd8ee30a860e15c2b9c48e2a8a53e5
vbendre <vbendre@outlook.com>
2020-08-30 10:44:40 -0700

    added custome namespace and repplication controller examples

--- grpc/webapp/main.go
+++ grpc/webapp/main.go
@@ -5,15 +5,35 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	x := 1
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		name, _ := os.Hostname()
+		fmt.Fprintf(w, "HostName %q", name)
 		fmt.Fprintf(w, "Hello %q", html.EscapeString(r.URL.Path))
+
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hi")
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		x = x + 1
+		fmt.Fprintf(w, "healthprobe %v ", x)
+		if x%5 == 0 {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "unhealthy")
+		} else {
+			w.WriteHeader(http.StatusAccepted)
+			fmt.Fprintf(w, "yaayy healthy")
+		}
+
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
